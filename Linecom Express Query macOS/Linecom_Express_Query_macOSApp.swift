ee3e423ee9339c32b65586703b85216f41c2f0d4
Linecom-lik <3448336734@qq.com>
2024-10-11 20:58:38 +0800

    Devide macos scheme

---
+++ Linecom Express Query macOS/Linecom_Express_Query_macOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  Linecom_Express_Query_macOSApp.swift
+//  Linecom Express Query macOS
+//
+//  Created by 程炜栋 on 2024/10/11.
+//
+
+import SwiftUI
+
+@main
+struct Linecom_Express_Query_macOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
