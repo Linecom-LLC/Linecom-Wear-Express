b66733f6bda6bdbc030f6bebdc4c7f9ea0d2d9ac
Linecom-lik <3448336734@qq.com>
2024-10-24 21:47:07 +0800

    Update Query Methods.

--- Linecom China Post Express Query/ExpressView.swift
+++ Linecom China Post Express Query/ExpressView.swift
@@ -60,8 +60,49 @@ struct ExpressView: View {
                 Text("正在查询...")
             }
             .onAppear() {
-                getcsrfToken()
-                doQuery()
+                let endpoint = "https://api.linecom.net.cn/express/token"
+                LinecomKit.NetworkAction.shared.requestString(endpoint) { resp, successd in
+                    if successd {
+                        token = resp
+                        
+                        let requrl = "https://www.kuaidi100.com/query?type=\(Provider)&postid=\(nu)&temp=0.\(Int64.random(in: 1000000000000000...9999999999999999))&phone=\(phone)"
+                        print(requrl)
+                        let Header: HTTPHeaders = ["Host": "www.kuaidi100.com",
+                                                   "Accept": "application/json, text/javascript, */*; q=0.01",
+                                                   "Sec-Fetch-Site": "same-origin",
+                                                   "Sec-Fetch-Mode": "cors",
+                                                   "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/18.1 Safari/605.1.15",
+                                                   "Referer": "https://www.kuaidi100.com/",
+                                                   "Sec-Fetch-Dest": "empty",
+                                                   "X-Requested-With": "XMLHttpRequest",
+                                                   "Accept-Language": "zh-CN,zh-Hans;q=0.9",
+                                                   "Priority": "u=3, i",
+                                                   "Accept-Encoding": "gzip, deflate, br",
+                                                   "Cookie": "csrftoken=\(token); _adadqeqwe1321312dasddocHref=; _adadqeqwe1321312dasddocReferrer=; _adadqeqwe1321312dasddocTitle=kuaidi100; snt_query_meta=%7B%22date%22%3A%222024830%22%2C%22nums%22%3A%5B%22SF3108015968802%22%5D%7D; WWWID=WWW91C3E5FEFAE299935E504EF29BC852ED; userRouteID=172767247932793238; sortStatus=0"]
+                            LinecomKit.NetworkAction.shared.requestJSON(requrl, headers: Header) { resp, successd in
+                                if successd {
+                                    nowState = resp["state"].string ?? "0"
+                                    errormsg = resp["message"].string ?? ""
+                                    
+                                    // 解析 JSON 数据
+                                    for item in resp["data"].arrayValue {
+                                        let deliveryItem = DeliveryItem(
+                                            time: item["time"].stringValue,
+                                            context: item["context"].stringValue,
+                                            location: item["location"].stringValue
+                                        )
+                                        deliveryItems.append(deliveryItem) // 将解析的数据添加到数组中
+                                    }
+                                    isLoading = false
+                                    status = "200"
+                                    
+                                } else {
+                                    isLoading = false
+                                    status = "500"
+                                }
+                            }
+                    }
+                }
             }
         } else if status != "200" {
             List {
@@ -87,54 +128,4 @@ struct ExpressView: View {
     //    ExpressView()
     //}
     
-    func getcsrfToken() {
-        let endpoint = "https://api.linecom.net.cn/express/token"
-        LinecomKit.NetworkAction.shared.requestString(endpoint) { resp, successd in
-            if successd {
-                token = resp
-            }
-        }
-    }
-    
-    func doQuery() {
-        let requrl = "https://www.kuaidi100.com/query?type=\(Provider)&postid=\(nu)&temp=0.\(Int64.random(in: 1000000000000000...9999999999999999))&phone=\(phone)"
-        print(requrl)
-        let Header: HTTPHeaders = ["Host": "www.kuaidi100.com",
-                                   "Accept": "application/json, text/javascript, */*; q=0.01",
-                                   "Sec-Fetch-Site": "same-origin",
-                                   "Sec-Fetch-Mode": "cors",
-                                   "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/18.1 Safari/605.1.15",
-                                   "Referer": "https://www.kuaidi100.com/",
-                                   "Sec-Fetch-Dest": "empty",
-                                   "X-Requested-With": "XMLHttpRequest",
-                                   "Accept-Language": "zh-CN,zh-Hans;q=0.9",
-                                   "Priority": "u=3, i",
-                                   "Accept-Encoding": "gzip, deflate, br",
-                                   "Cookie": "csrftoken=\(token); _adadqeqwe1321312dasddocHref=; _adadqeqwe1321312dasddocReferrer=; _adadqeqwe1321312dasddocTitle=kuaidi100; snt_query_meta=%7B%22date%22%3A%222024830%22%2C%22nums%22%3A%5B%22SF3108015968802%22%5D%7D; WWWID=WWW91C3E5FEFAE299935E504EF29BC852ED; userRouteID=172767247932793238; sortStatus=0"]
-//        if tokenstate {
-            LinecomKit.NetworkAction.shared.requestJSON(requrl, headers: Header) { resp, successd in
-                if successd {
-                    nowState = resp["state"].string ?? "0"
-                    status = resp["status"].string ?? "10"
-                    errormsg = resp["message"].string ?? ""
-                    
-                    // 解析 JSON 数据
-                    for item in resp["data"].arrayValue {
-                        let deliveryItem = DeliveryItem(
-                            time: item["time"].stringValue,
-                            context: item["context"].stringValue,
-                            location: item["location"].stringValue
-                        )
-                        deliveryItems.append(deliveryItem) // 将解析的数据添加到数组中
-                    }
-                    isLoading = false
-                    
-                    
-                } else {
-                    status = "10"
-                    isLoading = false
-                }
-            }
-//        }
-    }
 }
